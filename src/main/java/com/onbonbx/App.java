9da35a6f3873bed7da67741ae84eee04e974908d
wangyq <wangyq@onbonbx.com>
2020-01-23 16:48:36 +0800

    add app.java

---
+++ src/main/java/com/onbonbx/App.java
@@ -0,0 +1,13 @@
+package com.onbonbx;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/onbonbx/AppTest.java
@@ -0,0 +1,30 @@
+package com.onbonbx;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void testTwo() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testThree() {
+        assertTrue(true);
+    }
+}
